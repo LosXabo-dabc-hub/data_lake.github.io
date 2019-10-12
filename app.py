fd87916803325224cfbb7688a3c232a414148c0e
Mike Walsh <dabc@cox.net>
2019-10-12 14:22:03 -0400

    upd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scrape_quakes
 
@@ -16,15 +16,36 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 # Route to render news1.html template using data from Mongo
 @app.route("/")
 def home():
+    # Return template and data
+    return render_template("index.html")
+
+
+@app.route("/news2")
+def news2():
+    return render_template("news2.html")
+
+@app.route("/news1")
+def news1():
 
     # Find one record of data from the mongo database
     mission_data = mongo.db.collection.find_one()
 
     # Return template and data
-    if mission_data: 
+    if mission_data:
+        print("!!!")
         return render_template("news1.html", trek=mission_data)
     else:
         return redirect("/scrape")
+
+
+    #  return render_template("news1.html")
+    # Find one record of data from the mongo database
+    #mission_data = mongo.db.collection.find_one()
+    #if mission_data: 
+    #return app.send_static_file("templates/news1.html")
+    #else:
+        #return redirect("/scrape")
+
 
 
 # Route that will trigger the scrape function
@@ -38,7 +59,7 @@ def scrape():
     mongo.db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
-    return redirect("/")
+    return redirect("/news1")
 
 
 if __name__ == "__main__":
